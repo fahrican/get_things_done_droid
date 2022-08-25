5d57e9926e8798d16645f827455b51434e791ae2
fahri <fahri-can@gmx.at>
2022-08-25 22:55:31 +0200

    Create alert dialog.

--- app/src/main/java/com/justluxurylifestyle/get_things_done_droid/ui/TaskDetailFragment.kt
+++ app/src/main/java/com/justluxurylifestyle/get_things_done_droid/ui/TaskDetailFragment.kt
@@ -1,13 +1,17 @@
 package com.justluxurylifestyle.get_things_done_droid.ui
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.justluxurylifestyle.get_things_done_droid.R
 import com.justluxurylifestyle.get_things_done_droid.core.ViewBindingFragment
 import com.justluxurylifestyle.get_things_done_droid.databinding.FragmentTaskDetailBinding
 import com.justluxurylifestyle.get_things_done_droid.viewmodel.TaskViewModel
@@ -35,10 +39,28 @@ class TaskDetailFragment : ViewBindingFragment<FragmentTaskDetailBinding>() {
         val id: String = args.taskItem.id.toString()
 
         binding.deleteTaskBtn.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.Main) {
-                async { viewModel.deleteTask(id) }.await()
-                findNavController().popBackStack()
-            }
+            displayAlertDialog(id)
         }
+    }
+
+    private fun displayAlertDialog(id: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage(getString(R.string.delete_task_headline))
+            .setPositiveButton(
+                "delete"
+            ) { _, _ ->
+                lifecycleScope.launch(Dispatchers.Main) {
+                    async { viewModel.deleteTask(id) }.await()
+                    findNavController().popBackStack()
+                }
+                Toast.makeText(requireContext(), "pressed delete", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton(
+                "cancel"
+            ) { _, _ ->
+                Toast.makeText(requireContext(), "pressed cancel", Toast.LENGTH_SHORT).show()
+            }
+        builder.create()
+        builder.show()
     }
 }
