8acffde00c1c7967556f05cf43ae6ea092b1c788
fahri <fahri-can@gmx.at>
2022-08-26 23:11:17 +0200

    Start to display edit tasks value.

--- app/src/main/java/com/justluxurylifestyle/get_things_done_droid/ui/EditTaskFragment.kt
+++ app/src/main/java/com/justluxurylifestyle/get_things_done_droid/ui/EditTaskFragment.kt
@@ -4,23 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.justluxurylifestyle.get_things_done_droid.core.ViewBindingFragment
-import com.justluxurylifestyle.get_things_done_droid.databinding.FragmentCreateTaskBinding
+import com.justluxurylifestyle.get_things_done_droid.databinding.FragmentEditTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class EditTaskFragment : ViewBindingFragment<FragmentCreateTaskBinding>() {
+class EditTaskFragment : ViewBindingFragment<FragmentEditTaskBinding>() {
+
+    private val args: EditTaskFragmentArgs by navArgs()
 
     override fun createBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentCreateTaskBinding = FragmentCreateTaskBinding.inflate(inflater)
+    ): FragmentEditTaskBinding = FragmentEditTaskBinding.inflate(inflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.task = args.taskItem
     }
-
 }
