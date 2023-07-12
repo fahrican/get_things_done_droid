e9c6b8468f2f9c49cac596c16dc7f74b274f4f6a
Habibi Coding <habibicoding@aol.com>
2023-07-12 19:44:38 +0200

    Remove unnecessary type.

--- app/src/main/java/com/justluxurylifestyle/get_things_done_droid/model/Priority.kt
+++ app/src/main/java/com/justluxurylifestyle/get_things_done_droid/model/Priority.kt
@@ -1,5 +1,5 @@
 package com.justluxurylifestyle.get_things_done_droid.model
 
-enum class Priority(type: String) {
-    LOW("LOW"), MEDIUM("MEDIUM"), HIGH("MEDIUM")
+enum class Priority {
+    LOW, MEDIUM, HIGH
 }
