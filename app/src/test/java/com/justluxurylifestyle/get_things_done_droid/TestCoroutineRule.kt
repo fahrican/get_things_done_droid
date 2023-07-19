28556b98a6893f464f9658d776275e33c9f7cd83
Habibi Coding <habibicoding@aol.com>
2023-07-19 18:24:25 +0200

    More improvements.

--- app/src/test/java/com/justluxurylifestyle/get_things_done_droid/TestCoroutineRule.kt
+++ app/src/test/java/com/justluxurylifestyle/get_things_done_droid/TestCoroutineRule.kt
@@ -18,22 +18,20 @@ class TestCoroutineRule : TestRule {
 
     private val testCoroutineScope = TestScope(testCoroutineDispatcher)
 
-    override fun apply(base: Statement, description: Description): Statement = object : Statement() {
-        @Throws(Throwable::class)
-        override fun evaluate() {
-            Dispatchers.setMain(testCoroutineDispatcher)
+    override fun apply(base: Statement, description: Description): Statement =
+        object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                Dispatchers.setMain(testCoroutineDispatcher)
 
-            base.evaluate()
+                base.evaluate()
 
-            Dispatchers.resetMain()
+                Dispatchers.resetMain()
+
+                runBlockingTest { testCoroutineScope }
+            }
         }
-    }
 
     fun runBlockingTest(block: suspend TestScope.() -> Unit) =
-        testCoroutineScope.runTest { block() }
-
-    private fun block() {
-        TODO("Not yet implemented")
-    }
-
+        testCoroutineScope.runTest { block }
 }
